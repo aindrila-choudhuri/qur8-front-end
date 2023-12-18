0424184e85a66bb443233d8dde56736764fafdb2
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2023-12-18 16:19:12 +0530

    improving file structure

--- app/index.js
+++ app/index.js
@@ -2,15 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
 import { SafeAreaView, StyleSheet, View, Text } from "react-native";
 import { COLORS, FONT, SIZES } from "../constants";
-import SignIn from "../app/screens/signin";
-import Otp from "../app/screens/otp";
-import { store } from "./src/redux/store";
+
+import { store } from "../app/redux/store"
 
 export default function Page() {
   return (
     <>
       <Provider store={store}>
         <AppRoute />
+        
         <StatusBar style="auto" />
       </Provider>
     </>
